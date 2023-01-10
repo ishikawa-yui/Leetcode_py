5f50be8e412c66938a6c50f8dea16f715682bfd4
イシカワ __ ユイ <71602299+ishikawa-yui@users.noreply.github.com>
2023-01-10 12:47:30 +0530

    Add files via upload

---
+++ #1. Two Sum (Easy).py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        prevmap = {} #hashmap following val : index entry
+
+        #we traverse thru the array and keep adding the visited elements in the hashmap
+
+        for i, n in enumerate(nums):
+            diff = target - n #to get the only number we need from the array
+            if diff in prevmap:
+                return [prevmap[diff], i] #return the index of the elements that add up to
+                                          #the value
+            prevmap[n] = i  #if sol not found then update the hashmap
+        return ":)"
