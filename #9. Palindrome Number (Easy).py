8a63d1db8859dd94593c5a5cc17b8d80bdd2eb3f
イシカワ __ ユイ <71602299+ishikawa-yui@users.noreply.github.com>
2023-01-10 12:49:01 +0530

    Add files via upload

---
+++ #9. Palindrome Number (Easy).py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        sum = 0
+        temp = x
+        while(x>0):
+            r = x % 10 # here r stores the remainder
+            sum = (sum * 10) + r 
+            x = x // 10
+
+        if(temp == sum):
+            return True
+        else:
+            return False
