369a844b4d45ed31926112236e9712fd9deada55
イシカワ __ ユイ <71602299+ishikawa-yui@users.noreply.github.com>
2023-01-11 14:20:05 +0530

    Update #49. Group Anagrams (Medium).py

--- #49. Group Anagrams (Medium).py
+++ #49. Group Anagrams (Medium).py
@@ -1,17 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        #49. Group Anagrams (Medium)
+         #49. Group Anagrams (Medium)
 
-        res = defaultdict(list) #we map charcount to the list of anagrams
+        ans = collections.defaultdict(list) #we map charcount to the list of anagrams
         # for ex ant and tan have the same list of characters a, n and t so we use this as a key 
         # and the n we use the list of anagrams as the value
 
         for s in strs:
-            count = [0] * 26 # for a ... z
+            count = [0] * 26# for a ... z
 
             for c in s:
-                #here we are mapping the lowercase letters from 0 and not their ASCII values 
-                count[ord(c) - ord("a")] +=1 #so to get the value of c from 0 we subtract the ASCII value of "a" from it
-                res[tuple(count)].append(s)
-
-            return res.values()
+                #here we are mapping the lowercase letters from 0 and not their ASCII value
+                count[ord(c) - ord("a")] += 1 #so to get the value of c from 0 we subtract the ASCII value of "a" from it
+            ans[tuple(count)].append(s)
+        return ans.values()
